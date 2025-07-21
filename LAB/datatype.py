aca53b78c61486adb79cdeb40fb337944381402c
Atharv2104 <liteatharv4@gmail.com>
2025-07-21 09:57:36 +0530

    datatype

---
+++ LAB/datatype.py
@@ -0,0 +1,29 @@
+print("data types in python")
+
+a=10
+print("integer:",a)
+
+pi=3.14
+print("float:",pi)
+
+str="hello"
+print("string:",str)
+
+x=["apple","bannana","cherry"]
+print("list:",x)
+
+y=("apple","bannana","cherry")
+print("tuple:",y)
+
+z={"name":"john","age":20,"city":"mumbai"}
+print("dictionary:",z)
+
+s={a,2,3,4,5}
+print("set:",s)
+
+b=True
+print("boolean;",b)
+
+c=None
+print("None Type:",c)
+
